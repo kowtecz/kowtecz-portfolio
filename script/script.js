7c047aef01f074cf673f6d5420bcc3c6acd7a986
eduardxdc <eduardxdc@gmail.com>
2024-02-02 14:19:11 -0300

    initial commit

---
+++ script/script.js
@@ -0,0 +1,47 @@
+var Botao = document.getElementById("btnPatas");
+var urlDeDestino = "https://patasecompanhia.pt";
+Botao.addEventListener("click", function() {
+ window.location.href = urlDeDestino;
+});
+
+var BotaoDois = document.getElementById("btnRaff");
+var urlDeDestinoDois = "https://www.raffcom.com.br/";
+BotaoDois.addEventListener("click", function() {
+ window.location.href = urlDeDestinoDois;
+});
+
+var BotaoTres = document.getElementById("btnGuri");
+var urlDeDestinoTres = "https://www.behance.net/gallery/170760271/BrandBook-Embalagem-de-Picol-para-Sorvetes-Guri";
+BotaoTres.addEventListener("click", function() {
+ window.location.href = urlDeDestinoTres;
+});
+
+var BotaoQuatro = document.getElementById("btnVv");
+var urlDeDestinoQuatro = "https://www.behance.net/gallery/165893149/Victor-Vinicius-Manual-de-Marca";
+BotaoQuatro.addEventListener("click", function() {
+ window.location.href = urlDeDestinoQuatro;
+});
+
+var BotaoCinco = document.getElementById("btnAciu");
+var urlDeDestinoCinco = "https://www.behance.net/gallery/180381771/Dia-da-arvore-Post-social-media-(ACIU)";
+BotaoCinco.addEventListener("click", function() {
+ window.location.href = urlDeDestinoCinco;
+});
+
+var BotaoSeis = document.getElementById("btnSantos");
+var urlDeDestinoSeis = "https://www.behance.net/gallery/178844439/MATCHDAY-FOOTBALL-001-Santos-Sub-20";
+BotaoSeis.addEventListener("click", function() {
+ window.location.href = urlDeDestinoSeis;
+});
+
+var BotaoSete = document.getElementById("btnOzi");
+var urlDeDestinoSete = "https://www.behance.net/gallery/174052245/Ozi-Tracker-Vr-Banners";
+BotaoSete.addEventListener("click", function() {
+ window.location.href = urlDeDestinoSete;
+});
+
+var BotaoOito = document.getElementById("btnAfa");
+var urlDeDestinoSete = "https://drive.google.com/file/d/1uV8R1J_nHfLcizYwPooQl4Rq89FmSQmf/view?usp=sharing";
+BotaoSete.addEventListener("click", function() {
+ window.location.href = urlDeDestinoOito;
+});
